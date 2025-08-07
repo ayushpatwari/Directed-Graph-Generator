af136b1a0ae8533ef3fcdacb2535bc5f2ce86d8c
Ayush Patwari <ayushvpatwari@gmail.com>
2025-08-07 19:00:52 -0400

    declutted repo

--- sample_graph.py
+++ sample_graph.py
@@ -13,13 +13,18 @@ if __name__ == "__main__":
 
     """Replace with (start_node, end_node)"""
     connections = [
-        # (1, 3),
-        # (3, 4)
+        (1, 3),
+        (3, 4),
+        (2, 2)
     ]
 
     graph = DirectedGraph(nodes, connections)
 
     graph.add_connection(1, 2)
+    graph.add_connection(2, 3)
     graph.add_node(10)
+    graph.add_connection(10, 4)
+    graph.add_connection(7,3)
+    graph.add_connection(10, 1)
 
     graph.generate_graph()
