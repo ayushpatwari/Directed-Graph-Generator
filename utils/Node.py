2bd77861c0c1779ede03a22c831ddd41017e857f
Ayush Patwari <ayushvpatwari@gmail.com>
2025-08-13 21:43:55 -0400

    added explicit return types for Node.py

--- utils/Node.py
+++ utils/Node.py
@@ -7,7 +7,7 @@ class Node:
         self.connection_count = 0
     
     
-    def change_value(self, new_value):
+    def change_value(self, new_value) -> bool:
         """
         Changes value of node
             
@@ -15,24 +15,25 @@ class Node:
             new_value(value): new value to insert into node
         
         Returns:
-            bool: operation success
+            bool: whether the replacement of value was successful
         """
         self.value = new_value
         return True
     
-    def remove_value(self):
+    def remove_value(self) -> bool:
         """
         Removes value from node
         
         Returns:
-            bool: operation success
+            bool: whether removing the value was successful
         """
         self.value = None
     
-    def add_connection_count(self):
+    def add_connection_count(self) -> bool:
         self.connection_count += 1
+        return True
 
-    def __str__(self):
+    def __str__(self) -> str:
         """
         Expresses node through its value
         
