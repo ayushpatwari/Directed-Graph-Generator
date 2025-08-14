439a11dd74f3af6f6e5fcdd24012c0afff3f0e4f
Ayush Patwari <ayushvpatwari@gmail.com>
2025-08-13 21:43:42 -0400

    fixed syntax issues in NodeConnection.py and added documentation

--- utils/NodeConnection.py
+++ utils/NodeConnection.py
@@ -1,12 +1,36 @@
 from Node import Node
+
 class NodeConnection:
-    def __init__(self, Node start_node, Node end_node):
+    """"
+    This is a class for a connection between nodes in a graph
+    """
+    def __init__(self, start_node: Node, end_node: Node):
         self.start_node = start_node
         self.end_node = end_node
 
-    def change_start_node(Node new_start_node):
+    def change_start_node(self, new_start_node: Node) -> bool:
+        """
+        Changes start node of the connection
+        
+        Args:
+            new_start_node(Node): new start node
+        
+        Returns:
+            bool: whether the replacement of start node was successful
+        """
         self.start_node = new_start_node
+        return True
     
-    def change_end_node(Node new_end_node):
+    def change_end_node(self, new_end_node: Node):
+        """"
+        Changes end node of the connection
+        
+        Args:
+            new_end_node(Node): new end node
+        
+        Returns:
+            bool: whether the replacement of end node was successful
+        """
         self.end_node = new_end_node
+        return True
     
